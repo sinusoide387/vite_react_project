582908ffa02ec897bd473f4e5a631c40a8b777f5
Franco <franco_t92@hotmail.com>
2024-07-15 21:49:08 -0400

    vite project build

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import Header from "./Header";
+// import Main from "./Main";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <p>This is my first Vite project!</p>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <div>
+      <h1>Hello Vite!</h1>
+    </div>
+  );
+}
+
+export default Header;
